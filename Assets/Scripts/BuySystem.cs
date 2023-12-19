e28364b9362f899913b2a8652d2e57c4c80e5211
MertColakk <meertcolak06@gmail.com>
2023-12-19 18:33:11 +0300

    Bug on button click fixed. Coin Rate upgrade has been added.

--- Assets/Scripts/BuySystem.cs
+++ Assets/Scripts/BuySystem.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class BuySystem : MonoBehaviour
 {
     //Variables      
+    bool isOpen=false;
+
+    //Gold variables
+    float neededGold=120;
+    int level=0;
+    [SerializeField] Text costText;
+    [SerializeField] Text levelText;
+
+    //Others
     MeteorAttribute meteorAttribute;
     CarbonStone carbonStone;
     HabitabilityBarSystem habitabilitySystem;
@@ -19,11 +30,35 @@ public class BuySystem : MonoBehaviour
             gemStone = planet.GetComponent<GemStone>();
             habitabilitySystem = planet.GetComponent<HabitabilityBarSystem>();
             coinManager = planet.GetComponent<CoinManager>();
-            panel.gameObject.SetActive(false);
+            panel.gameObject.SetActive(isOpen);
+            updateText(costText,neededGold);
         }
     }
 
-    
+    //Panel functions
+    public void changePanel(){
+        isOpen = !isOpen;
+
+        panel.gameObject.SetActive(isOpen);
+    }
+
+    void updateText(Text text,float amount){
+        text.text = amount.ToString();
+    }
+    void updateText(Text text,int amount){
+        text.text = "Level: "+amount.ToString();
+    }
+
+    public void upgradeCoin(){
+        if(coinManager.coin>=neededGold){
+            coinManager.coin-= neededGold;
+            coinManager.coinPerSecond *= 1.6f;
+            neededGold *= 1.8f;
+            level++;
+            updateText(levelText,level);
+        }
+        updateText(costText,neededGold);
+    }
 
     
 
