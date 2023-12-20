1bc3cb05ba4eb798c1667a24ed1750cb63ee4de3
MertColakk <meertcolak06@gmail.com>
2023-12-21 02:22:57 +0300

    Prefabs has been changed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float rotateSpeed = 30f; 
-    [SerializeField] List<GameObject> phases;
     HabitabilityBarSystem system;
+    GameObject asset,asset1,asset2;
  
     void Start(){
         GameObject buySystem = GameObject.Find("BuySystem");
         if(buySystem!=null)
             system = buySystem.GetComponent<HabitabilityBarSystem>();
+
+        asset = transform.GetChild(0).gameObject;
+        asset1 = transform.GetChild(1).gameObject;
+        asset2 = transform.GetChild(2).gameObject;
+
+        asset1.SetActive(false);
+        asset2.SetActive(false);
     }
     void Update()
     {
         //Rotate in the axis y like planet
-        transform.Rotate(0,rotateSpeed*Time.deltaTime,0);       
+        transform.Rotate(0,rotateSpeed*Time.deltaTime,0);
+    }
 
+    void FixedUpdate(){
+        if(system.habitability>=750&&asset.activeSelf){
+            asset.SetActive(false);
+            asset1.SetActive(true);
+        }else if(system.habitability>=1500&&asset1.activeSelf){
+            asset1.SetActive(false);
+            asset2.SetActive(true);
+        }
     }
 
 
@@ -27,5 +43,7 @@ public class PlayerController : MonoBehaviour
         other.gameObject.SetActive(false);
                                 
     }
+
+    
 
 }
