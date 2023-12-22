b35ca95975df947c82a115966329a561654fb00b
MertColakk <meertcolak06@gmail.com>
2023-12-23 00:37:40 +0300

    UI Changed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,18 @@ public class PlayerController : MonoBehaviour
 {
     float rotateSpeed = 30f; 
     HabitabilityBarSystem system;
+    EffectSystem effects;
     GameObject asset,asset1,asset2;
-    public GameObject changePrefab;
  
     void Start(){
         GameObject buySystem = GameObject.Find("BuySystem");
-        if(buySystem!=null)
+        GameObject effectSystem = GameObject.Find("EffectSystem");
+        if(buySystem!=null&&effectSystem!=null){
             system = buySystem.GetComponent<HabitabilityBarSystem>();
+            effects = effectSystem.GetComponent<EffectSystem>();
+        }
+            
+            
 
         asset = transform.GetChild(0).gameObject;
         asset1 = transform.GetChild(1).gameObject;
@@ -32,11 +37,11 @@ public class PlayerController : MonoBehaviour
         if(system.habitability>=750&&asset.activeSelf){
             asset.SetActive(false);
             asset1.SetActive(true);
-            Instantiate(changePrefab,transform.position,Quaternion.identity);
+            Instantiate(effects.effectsPrefab[8],transform.position,Quaternion.identity);
         }else if(system.habitability>=1500&&asset1.activeSelf){
             asset1.SetActive(false);
             asset2.SetActive(true);
-            Instantiate(changePrefab,transform.position,Quaternion.identity);
+            Instantiate(effects.effectsPrefab[8],transform.position,Quaternion.identity);
         }
     }
 
